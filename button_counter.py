5795d6ed97a1c85339576230bd29f433d360ce95
mhmoraes <mhdmoraes@gmail.com>
2015-02-16 19:55:56 +0000

    first commit

--- button_counter.py
+++ button_counter.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.IN)
+GPIO.setup(12, GPIO.IN) #set pin 12 as input
 time_pressed = 0
 counter = 0
 
-def clear_counter():
+def clear_counter(): #function to clear counter if button pressed for 3 seconds
     global counter
     if time_pressed > 15:
         counter = 0
@@ -14,22 +14,22 @@ def clear_counter():
     else:
         return counter
     
-while True:
-    input_value=GPIO.input(12)
+while True: #loop to keep reading input
+    input_value=GPIO.input(12)#set variable with input value, FALSE = pressed
     if input_value==False:
         counter +=1
         time_pressed = 0
-        while input_value== False:
+        while input_value== False: #loop to keep reading input to count how long the button is pressed
             
             time_pressed= time_pressed+1
             input_value=GPIO.input(12)
-            time.sleep(0.05)
-            counter = clear_counter()
-    elif input_value==True:
+            time.sleep(0.05) #set time loop is repeated 
+            counter = clear_counter() #run fun. clear_counter
+    elif input_value==True: #run if button NOT pressed
         
-        print "counter =", counter
-        while input_value== True:
+        print "counter =", counter #print how many times button was pressed
+        while input_value== True: #loop to keep reading input if button not pressed 
             input_value=GPIO.input(12)
-GPIO.cleanup()    
+GPIO.cleanup()    #clear GPIO
 
 
